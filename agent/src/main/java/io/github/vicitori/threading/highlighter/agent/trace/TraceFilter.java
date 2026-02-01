2ba3f73f11f2a62988388bfa75c8b4a17c45fba1
Victoria Ostrovskaya <victoria.v.ostrovskaya@gmail.com>
2026-02-01 12:39:37 +0300

    feat: markers not only from the user project

--- agent/src/main/java/io/github/vicitori/threading/highlighter/agent/trace/TraceFilter.java
+++ agent/src/main/java/io/github/vicitori/threading/highlighter/agent/trace/TraceFilter.java
@@ -1,24 +1,24 @@
-package io.github.vicitori.threading.highlighter.agent.trace;
-
-public final class TraceFilter {
-    private static final String USER_PACKAGE_PROPERTY = "threading.highlighter.user.package";
-
-    private final String userPackagePrefix;
-
-    public TraceFilter() {
-        String packageName = System.getProperty(USER_PACKAGE_PROPERTY);
-        this.userPackagePrefix = packageName.trim();
-        System.err.println("[TraceFilter] Filtering enabled for package: " + userPackagePrefix);
-    }
-
-    public boolean containsUserCode(StackTraceElement[] stackTrace) {
-        for (StackTraceElement element : stackTrace) {
-            String className = element.getClassName();
-            if (className.startsWith(userPackagePrefix)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-}
+//package io.github.vicitori.threading.highlighter.agent.trace;
+//
+//public final class TraceFilter {
+//    private static final String USER_PACKAGE_PROPERTY = "threading.highlighter.user.package";
+//
+//    private final String userPackagePrefix;
+//
+//    public TraceFilter() {
+//        String packageName = System.getProperty(USER_PACKAGE_PROPERTY);
+//        this.userPackagePrefix = packageName.trim();
+//        System.err.println("[TraceFilter] Filtering enabled for package: " + userPackagePrefix);
+//    }
+//
+//    public boolean containsUserCode(StackTraceElement[] stackTrace) {
+//        for (StackTraceElement element : stackTrace) {
+//            String className = element.getClassName();
+//            if (className.startsWith(userPackagePrefix)) {
+//                return true;
+//            }
+//        }
+//
+//        return false;
+//    }
+//}
