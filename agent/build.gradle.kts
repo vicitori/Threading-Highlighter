c6acec4d33fa559a93217e54ff1e11038abd62c1
Victoria Ostrovskaya <victoria.v.ostrovskaya@gmail.com>
2026-02-11 17:57:29 +0300

    ref: formatter

--- agent/build.gradle.kts
+++ agent/build.gradle.kts
@@ -13,16 +13,17 @@ dependencies {
     implementation("net.bytebuddy:byte-buddy-agent:1.15.11")
 }
 
-tasks.shadowJar {
-    archiveClassifier.set("")
-
-    manifest {
-        attributes(
-            mapOf(
-                "Premain-Class" to "io.github.vicitori.threading.highlighter.agent.ThreadingHighlighterAgent",
-                "Can-Redefine-Classes" to "true",
-                "Can-Retransform-Classes" to "true"
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
+        manifest {
+            attributes(
+                mapOf(
+                    "Premain-Class" to "io.github.vicitori.threading.highlighter.agent.ThreadingHighlighterAgent",
+                    "Can-Redefine-Classes" to "true",
+                    "Can-Retransform-Classes" to "true"
+                )
             )
-        )
+        }
     }
 }
