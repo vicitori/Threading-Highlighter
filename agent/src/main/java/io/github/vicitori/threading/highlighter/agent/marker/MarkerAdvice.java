0591054cb9680b9c3699930638e2027f1b9de649
Victoria Ostrovskaya <victoria.v.ostrovskaya@gmail.com>
2026-02-11 17:57:29 +0300

    ref: formatter

--- agent/src/main/java/io/github/vicitori/threading/highlighter/agent/marker/MarkerAdvice.java
+++ agent/src/main/java/io/github/vicitori/threading/highlighter/agent/marker/MarkerAdvice.java
@@ -1,21 +1,22 @@
 package io.github.vicitori.threading.highlighter.agent.marker;
 
-import io.github.vicitori.threading.highlighter.agent.instruction.InstructionWriter;
+import io.github.vicitori.threading.highlighter.agent.trace.TraceWriter;
 import net.bytebuddy.asm.Advice;
 
 import java.lang.reflect.Method;
 
 public final class MarkerAdvice {
-    public static volatile InstructionWriter writer;
+    public static volatile TraceWriter writer;
     public static final ThreadLocal<Boolean> IN_RECORD = ThreadLocal.withInitial(() -> false);
 
-    public static void setWriter(InstructionWriter w) {
+    public static void setWriter(TraceWriter w) {
         writer = w;
     }
 
+    @SuppressWarnings("unused") // Called by Byte Buddy instrumentation
     @Advice.OnMethodEnter
     public static void onEnter(@Advice.Origin Method method) {
-        InstructionWriter w = writer;
+        TraceWriter w = writer;
         if (w == null) {
             return;
         }
@@ -27,7 +28,6 @@ public final class MarkerAdvice {
         try {
             Throwable stackTraceHolder = new Throwable();
             String markerFqn = method.getDeclaringClass().getName() + "#" + method.getName();
-            System.err.println("[MarkerAdvice] MARKER INTERCEPTED: " + markerFqn);
             w.record(markerFqn, stackTraceHolder);
         } finally {
             IN_RECORD.set(false);
