b18b178428513f8251b5756ffd0da4264ec2021e
Victoria Ostrovskaya <victoria.v.ostrovskaya@gmail.com>
2026-02-16 12:59:47 +0300

    chore: update plugin configuration

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm")
+    kotlin("plugin.serialization") version "2.2.21"
     id("org.jetbrains.intellij.platform")
 }
 
@@ -29,5 +30,11 @@ intellijPlatform {
             sinceBuild = "251"
             untilBuild = "253.*"
         }
+    }
+}
+
+tasks {
+    named("buildPlugin") {
+        dependsOn(":agent:shadowJar")
     }
 }
