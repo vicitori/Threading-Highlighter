3a75cd70af8f193f1322aa88126fce3eb4f025e0
Victoria Ostrovskaya <victoria.v.ostrovskaya@gmail.com>
2026-06-23 12:19:36 +0300

    ci: publish plugin to marketplace on version tag

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -38,6 +38,17 @@ intellijPlatform {
             recommended()
         }
     }
+
+    // Credentials are supplied by the CI `release` job via environment variables
+    signing {
+        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+        privateKey = providers.environmentVariable("PRIVATE_KEY")
+        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+    }
+
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
+    }
 }
 
 tasks {
