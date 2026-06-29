df33c03539b6b175f0c8ababfb3be98dba93e9c5
Victoria Ostrovskaya <victoria.v.ostrovskaya@gmail.com>
2026-06-29 09:38:26 +0300

    fix: declare background action update thread for actions

--- plugin/src/main/kotlin/io/github/vicitori/threading/highlighter/plugin/actions/ReloadTraceAction.kt
+++ plugin/src/main/kotlin/io/github/vicitori/threading/highlighter/plugin/actions/ReloadTraceAction.kt
@@ -2,11 +2,15 @@ package io.github.vicitori.threading.highlighter.plugin.actions
 
 import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import io.github.vicitori.threading.highlighter.plugin.services.TraceManager
 
 class ReloadTraceAction : AnAction() {
+
+    // update() only reads e.project, so it is safe to compute off the EDT
+    override fun getActionUpdateThread() = ActionUpdateThread.BGT
 
     override fun update(e: AnActionEvent) {
         val hasProject = e.project != null
--- plugin/src/main/kotlin/io/github/vicitori/threading/highlighter/plugin/actions/ShowTraceSummaryAction.kt
+++ plugin/src/main/kotlin/io/github/vicitori/threading/highlighter/plugin/actions/ShowTraceSummaryAction.kt
@@ -1,11 +1,15 @@
 package io.github.vicitori.threading.highlighter.plugin.actions
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import io.github.vicitori.threading.highlighter.plugin.services.TraceManager
 import io.github.vicitori.threading.highlighter.plugin.ui.TextInfoDialog
 
 class ShowTraceSummaryAction : AnAction() {
+
+    // update() only reads e.project, so it is safe to compute off the EDT
+    override fun getActionUpdateThread() = ActionUpdateThread.BGT
 
     override fun update(e: AnActionEvent) {
         val hasProject = e.project != null
